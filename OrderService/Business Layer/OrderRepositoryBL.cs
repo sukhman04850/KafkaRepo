91856a5db2341489491372b2bf4e2b938b52bd46
sukhmankochhar <sukhman.kochhar@nagarro.com>
2024-04-02 12:23:47 +0530

    test cases added

---
+++ OrderService/Business Layer/OrderRepositoryBL.cs
@@ -0,0 +1,45 @@
+﻿using OrderService.Interfaces;
+using OrderService.Model;
+using OrderService.Repositories;
+
+namespace OrderService.Business_Layer
+{
+    public class OrderRepositoryBL : IOrderRepositoryBL
+    {
+        private readonly IOrderInterface _orderRepo;
+        public OrderRepositoryBL(IOrderInterface orderRepo)
+        {
+            _orderRepo = orderRepo;
+
+        }
+
+        public async Task<Orders> AddOrder(Orders orderrequest)
+        {
+            var addOrder = await _orderRepo.AddOrder(orderrequest);
+            return addOrder;
+        }
+
+        public async Task DeleteOrder(int id)
+        {
+            await _orderRepo.DeleteOrder(id);
+        }
+
+        public async Task<List<Orders>> GetAllOrders()
+        {
+            var allOrder = await _orderRepo.GetAllOrders();
+            return allOrder;
+        }
+
+        public async Task<List<Orders>> GetOrderById(int id)
+        {
+            var orderId = await _orderRepo.GetOrderById(id);
+            return orderId;
+        }
+
+        public async Task<Orders> UpdateOrder(Orders orderupdate)
+        {
+            var updateOrder = await _orderRepo.UpdateOrder(orderupdate);
+            return updateOrder;
+        }
+    }
+}
